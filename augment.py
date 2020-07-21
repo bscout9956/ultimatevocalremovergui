4881ff80f1bd8bcf1e8af6a5e6c7be7d2d30738e
Anjok07 <68268275+Anjok07@users.noreply.github.com>
2020-07-20 23:51:39 -0500

    Add files via upload

--- augment.py
+++ augment.py
@@ -14,19 +14,20 @@ if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('--sr', '-r', type=int, default=44100)
     p.add_argument('--hop_length', '-l', type=int, default=1024)
-    p.add_argument('--pitch', '-p', type=int, default=-2)
-    p.add_argument('--mixture_dataset', '-m', required=True)
-    p.add_argument('--instrumental_dataset', '-i', required=True)
+    p.add_argument('--n_fft', '-f', type=int, default=2048)
+    p.add_argument('--pitch', '-p', type=int, default=-1)
+    p.add_argument('--mixtures', '-m', required=True)
+    p.add_argument('--instruments', '-i', required=True)
     args = p.parse_args()
 
     input_exts = ['.wav', '.m4a', '.3gp', '.oma', '.mp3', '.mp4']
     X_list = sorted([
-        os.path.join(args.mixture_dataset, fname)
-        for fname in os.listdir(args.mixture_dataset)
+        os.path.join(args.mixtures, fname)
+        for fname in os.listdir(args.mixtures)
         if os.path.splitext(fname)[1] in input_exts])
     y_list = sorted([
-        os.path.join(args.instrumental_dataset, fname)
-        for fname in os.listdir(args.instrumental_dataset)
+        os.path.join(args.instruments, fname)
+        for fname in os.listdir(args.instruments)
         if os.path.splitext(fname)[1] in input_exts])
 
     input_i = 'input_i_{}.wav'.format(args.pitch)
@@ -37,8 +38,22 @@ if __name__ == '__main__':
     cmd_v = 'soundstretch {} {} -pitch={}'.format(input_v, output_v, args.pitch)
     suffix = '_pitch{}.npy'.format(args.pitch)
 
+    outdir = 'sr{}_hl{}_nf{}'.format(args.sr, args. hop_length, args.n_fft)
+    mix_outdir = os.path.join(args.mixtures, outdir)
+    inst_outdir = os.path.join(args.instruments, outdir)
+    os.makedirs(mix_outdir, exist_ok=True)
+    os.makedirs(inst_outdir, exist_ok=True)
+
     filelist = list(zip(X_list, y_list))
     for mix_path, inst_path in tqdm(filelist):
+        mix_basename = os.path.splitext(os.path.basename(mix_path))[0]
+        inst_basename = os.path.splitext(os.path.basename(inst_path))[0]
+        mix_outpath = os.path.join(mix_outdir, mix_basename + suffix)
+        inst_outpath = os.path.join(inst_outdir, inst_basename + suffix)
+
+        if os.path.exists(mix_outpath) and os.path.exists(inst_outpath):
+            continue
+
         X, _ = librosa.load(
             mix_path, args.sr, False, dtype=np.float32, res_type='kaiser_fast')
         y, _ = librosa.load(
@@ -60,15 +75,11 @@ if __name__ == '__main__':
             output_v, args.sr, False, dtype=np.float32, res_type='kaiser_fast')
         X = y + v
 
-        spec = spec_utils.calc_spec(X, args.hop_length)
-        basename, _ = os.path.splitext(os.path.basename(mix_path))
-        outpath = os.path.join(args.mixture_dataset, basename + suffix)
-        np.save(outpath, np.abs(spec))
+        spec = spec_utils.get_spectrogram(X, args.hop_length, args.n_fft)
+        np.save(mix_outpath, spec)
 
-        spec = spec_utils.calc_spec(y, args.hop_length)
-        basename, _ = os.path.splitext(os.path.basename(inst_path))
-        outpath = os.path.join(args.instrumental_dataset, basename + suffix)
-        np.save(outpath, np.abs(spec))
+        spec = spec_utils.get_spectrogram(y, args.hop_length, args.n_fft)
+        np.save(inst_outpath, spec)
 
         os.remove(input_i)
         os.remove(input_v)
